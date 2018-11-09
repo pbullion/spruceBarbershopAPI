22c2636d180129bdc6a99bc2e9bd8e73bad8d94a
pbullion <pbullion@gmail.com>
2018-11-09 16:33:06 -0600

    Updats for aws and rds

---
+++ secrets/db_configuration.js
@@ -0,0 +1,7 @@
+module.exports = {
+    user: 'spruce_admin',
+    host: 'sprucebarbershopdb.cluysew5vnmx.us-west-2.rds.amazonaws.com',
+    database: 'postgres',
+    password: 'spruceBarberShop483904857238398475987349845738',
+    port: 5432
+};
