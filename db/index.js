a64bf295c5dc267353015601e03f37729bc9a463
pbullion <pbullion@gmail.com>
2018-11-09 16:46:47 -0600

    Test

--- db/index.js
+++ db/index.js
@@ -1,6 +1,14 @@
 const { Pool } = require('pg');
+/*
 const { user, host, database, password, port } = require('../secrets/db_configuration');
+*/
 
-const pool = new Pool({ user, host, database, password, port });
+const pool = new Pool({
+    user: 'spruce_admin',
+    host: 'sprucebarbershopdb.cluysew5vnmx.us-west-2.rds.amazonaws.com',
+    database: 'postgres',
+    password: 'spruceBarberShop483904857238398475987349845738',
+    port: 5432
+});
 
 module.exports = pool;
