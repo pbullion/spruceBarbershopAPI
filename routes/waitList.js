0f3a58684dd865ecc864c5ac13115b906169c00d
pbullion <pbullion@gmail.com>
2019-02-16 17:55:43 -0600

    Fixed waitlist bug

--- routes/waitList.js
+++ routes/waitList.js
@@ -78,7 +78,7 @@ router.delete('/:id', (request, response, next) => {
 router.post('/', (request, response, next) => {
     const join_time = moment().utcOffset('-06:00').format('HH:mm:ss');
     const todaysDate = moment().utcOffset('-06:00').format('L');
-    if (request.body.waitList.service2.id) {
+    if (request.body.waitList.service2) {
         pool.query(
             'INSERT INTO waitlist(userid, service1id, service2id, staffid, waiting, date, join_time, mobile_join) VALUES($1, $2, $3, $4, $5, $6, $7, $8)',
             [request.body.currentUser.id, request.body.waitList.service1.id, request.body.waitList.service2.id, request.body.waitList.staff.id, true, todaysDate, join_time, request.body.waitList.mobile_join],
